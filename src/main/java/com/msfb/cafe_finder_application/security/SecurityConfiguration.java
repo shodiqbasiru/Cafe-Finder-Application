75922776943146092034412649cbd05871e162bc
shodiqbasiru <shodiqbasiru@gmail.com>
2024-03-15 23:54:20 +0700

    [FEATURED] add security configuration

--- src/main/java/com/msfb/cafe_finder_application/security/SecurityConfiguration.java
+++ src/main/java/com/msfb/cafe_finder_application/security/SecurityConfiguration.java
@@ -1,4 +1,27 @@
 package com.msfb.cafe_finder_application.security;
 
+import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(cfg -> cfg.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req-> req.dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
+                        .anyRequest().permitAll())
+                .build();
+    }
 }
