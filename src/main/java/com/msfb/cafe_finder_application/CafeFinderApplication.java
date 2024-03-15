e6ab3bd33d962875232729dcd197bce386c6fa8d
shodiqbasiru <shodiqbasiru@gmail.com>
2024-03-15 21:16:54 +0700

    Initial commit

---
+++ src/main/java/com/msfb/cafe_finder_application/CafeFinderApplication.java
@@ -0,0 +1,13 @@
+package com.msfb.cafe_finder_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msfb/cafe_finder_application/CafeFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msfb.cafe_finder_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
