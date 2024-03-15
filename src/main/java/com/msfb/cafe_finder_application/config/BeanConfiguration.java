2a5db52c15e3b6ddbfd6a6f4de07d2d8aeafd176
shodiqbasiru <shodiqbasiru@gmail.com>
2024-03-15 23:49:18 +0700

    [FEATURED] add table constant for the name of the database

--- src/main/java/com/msfb/cafe_finder_application/config/BeanConfiguration.java
+++ src/main/java/com/msfb/cafe_finder_application/config/BeanConfiguration.java
@@ -1,2 +1,15 @@
-package com.msfb.cafe_finder_application.config;public class BeanConfiguration {
+package com.msfb.cafe_finder_application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanConfiguration {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 }
--- src/main/java/com/msfb/cafe_finder_application/constant/TableConstant.java
+++ src/main/java/com/msfb/cafe_finder_application/constant/TableConstant.java
@@ -1,4 +1,9 @@
 package com.msfb.cafe_finder_application.constant;
 
 public class TableConstant {
+    public static final String CAFE_TABLE = "tb_cafe";
+    public static final String MENU_TABLE = "tb_menu";
+    public static final String PRICE_TABLE = "tb_price";
+    public static final String REVIEW_TABLE = "tb_review";
+    public static final String IMAGE_TABLE = "tb_image";
 }
