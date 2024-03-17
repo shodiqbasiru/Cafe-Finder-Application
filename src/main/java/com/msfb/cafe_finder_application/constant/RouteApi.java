ba9ae6edcfe82023cd398b6be00ef0a67fa6a57c
shodiqbasiru <shodiqbasiru@gmail.com>
2024-03-17 21:39:22 +0700

    [FEATURE] add feat CRUD for Owner and User

--- src/main/java/com/msfb/cafe_finder_application/constant/RouteApi.java
+++ src/main/java/com/msfb/cafe_finder_application/constant/RouteApi.java
@@ -5,4 +5,6 @@ public class RouteApi {
     public static final String MENU_API = "/api/menus";
     public static final String REVIEW_API = "/api/reviews";
     public static final String AUTH_API = "/api/auth";
+    public static final String OWNER_API = "/api/owners";
+    public static final String USER_API = "/api/users";
 }
